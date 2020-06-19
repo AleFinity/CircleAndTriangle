b6f94769a0400a32fb320049edc42dfc94d800f2
AleFinity <51158743+AleFinity@users.noreply.github.com>
2020-06-19 15:17:05 +0300

    Add files via upload
    Напишите библиотеку для поставки внешним клиентам, которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам. Дополнительно к работоспособности:
    
    - Легкость добавления других фигур
    - Вычисление площади фигуры без знания типа фигуры
    - Проверку на то, является ли треугольник прямоугольным

---
+++ CircleAndTriangle/CircleAndTriangle/Areas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CircleAndTriangle
+{
+    public static class Areas
+    {
+        public static double Shape(double a, double b=0, double c=0)
+        {
+            if (c == 0 && b == 0) {
+                return CircleArea(a);
+            }
+            return TriangleArea(a, b, c);
+        }
+
+        public static double CircleArea(double radius) {
+            return Math.PI * radius * radius;
+        }
+        public static double TriangleArea(double a, double b, double c)
+        {
+            (double, double) tuple = RectangularTriangle(a, b, c);
+            if (tuple.Item1 != 0)
+                return tuple.Item1 * tuple.Item2 / 2;
+
+            double perimetr = (a + b + c)/2;
+            return Math.Sqrt(perimetr * (perimetr - a) * (perimetr - b) * (perimetr - c));
+        }
+        private static  (double, double) RectangularTriangle(double a, double b, double c) {
+            // If triangle is rectangular, we return its cathets.
+            if (a * a + b * b == c * c)
+                return (a, b);
+            else if (a * a + c * c == b * b)
+                return (a, c);
+            else if (b * b + c * c == a * a)
+                return (b, c);
+            return (0, 0);
+        }
+    }
+}
